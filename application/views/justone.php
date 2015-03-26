bbddaba5ddaa3d9ecf5b737c6e0dcc88c3f5c176
thunduaga <chriswumbojumbo@msn.com>
2015-03-26 10:39:41 -0700

    set up my justone view to have all the elements of an order and each burger
    10:39am
    Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

--- application/views/justone.php
+++ application/views/justone.php
@@ -1,3 +1,32 @@
 <div class="row">
-A burger?
+    <div class="container">
+        <h3>{customer} : {filename} : {ordertype}</h3>
+        <br>
+        Special: {special}
+        <br>
+            {burgers}
+            <br>
+            Patty: {patty}
+            <br>
+            Top Cheese: {cheeseTop}
+            <br>
+            Bottom Cheese: {cheeseBottom}
+            <br>
+            Toppings:
+            {toppings}
+                {topping}
+            {/toppings}
+            <br>
+            Sauces:
+            {sauces}
+                {sauce}
+            {/sauces}
+            <br>
+            Instructions: {instructions}
+            <br>
+            Name: {name}
+            <br>
+            <br>
+            {/burgers}
+    </div>
 </div>
