dfdbd2c737dac2985f2558a71a6df30789a539df
thunduaga <chriswumbojumbo@msn.com>
2015-03-26 16:09:58 -0700

    fixed the menu model
    4:09pm
    Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -11,24 +11,53 @@ class Menu extends CI_Model {
     protected $xml = null;
     protected $patty_names = array();
     protected $patties = array();
+    protected $cheeses = array();
+    protected $toppings = array();
+    protected $sauces = array();
 
     // Constructor
     public function __construct() {
         parent::__construct();
-        $xml = simplexml_load_file(DATAPATH . 'menu.xml');
+        $this->xml = simplexml_load_file(DATAPATH . 'menu.xml');
 
         // build the list of patties - approach 1
-        foreach ($xml->patties->patty as $patty) {
-            $patty_names[(string) $patty['code']] = (string) $patty;
+        foreach ($this->xml->patties->patty as $patty) {
+            $this->patty_names[(string) $patty['code']] = (string) $patty;
         }
 
         // build a full list of patties - approach 2
-        foreach ($xml->patties->patty as $patty) {
+        foreach ($this->xml->patties->patty as $patty) {
             $record = new stdClass();
             $record->code = (string) $patty['code'];
             $record->name = (string) $patty;
             $record->price = (float) $patty['price'];
-            $patties[$record->code] = $record;
+            $this->patties[$record->code] = $record;
+        }
+        //build a full list of cheeses
+        foreach ($this->xml->cheeses->cheese as $cheese) 
+        {
+            $record = new stdClass();
+            $record->code = (string) $cheese['code'];
+            $record->name = (string) $cheese;
+            $record->price = (float) $cheese['price'];
+            $this->cheeses[$record->code] = $record;
+        }
+        //build a full list of toppings
+        foreach ($this->xml->toppings->topping as $topping) 
+        {
+            $record = new stdClass();
+            $record->code = (string) $topping['code'];
+            $record->name = (string) $topping;
+            $record->price = (float) $topping['price'];
+            $this->toppings[$record->code] = $record;
+        }
+        //build a full list of sauces
+        foreach ($this->xml->sauces->sauce as $sauce) 
+        {
+            $record = new stdClass();
+            $record->code = (string) $sauce['code'];
+            $record->name = (string) $sauce;
+            $this->sauces[$record->code] = $record;
         }
     }
 
